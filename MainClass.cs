b804e612b5cc2427d5b77156546cbd7227eea631
NedimS <nedim.sehic@marvelsoft.net>
2020-04-04 15:41:27 +0200

    2020/04/04, Implement file streaming, fix Price validation

--- MainClass.cs
+++ MainClass.cs
@@ -1,79 +1,126 @@
 ﻿using static System.Console;
 using System.Text.RegularExpressions;
-using System.Reflection;
+using System.IO;
+using System.Threading;
+using System.Linq;
 
 public class MainClass {
 
     public static void Main(string[] args) {
         string instruction = "";
         Order order = new Order();
-        string[] lines = System.IO.File.ReadAllLines(@"FIXLog.txt");
 
         WriteLine("-----------------------------Execution Report---------------------------------");
         WriteLine(order);
         WriteLine("------------------------------------------------------------------------------\n");
         
-        for(int i = 0; i < lines.Length; ++i)
+        var path = @"FIXLog.txt";
+        var wh = new AutoResetEvent(false);
+        var fsw = new FileSystemWatcher(".");
+        
+        fsw.Filter = "*.txt";
+        fsw.EnableRaisingEvents = true;
+        fsw.Changed += (o,e) => wh.Set();
+
+        var lineCount = 0;
+        var lines = "";
+        
+        while(!string.Equals(instruction,"LOGOUT "))
         {
-        
-        string fixState = Regex.Match(lines[i],@"(?<=State: ).+?(?=\|)").ToString();
-        var fixQty = int.Parse(Regex.Match(lines[i],@"(?<=Quantity: ).+?(?=\|)").ToString());
-        var fixExecQty = int.Parse(Regex.Match(lines[i],@"(?<=Executed Quantity: ).+?(?=\|)").ToString());
-        var fixPrice = int.Parse(Regex.Match(lines[i],@"(?<=Price: ).+?(?=\|)").ToString());
-
-        
-        System.Console.WriteLine("State: " + fixState);
-        System.Console.WriteLine("Quantity: " + fixQty);
-        System.Console.WriteLine("Executed Quantity: " + fixExecQty);
-        System.Console.WriteLine("Price: " + fixPrice + " <========= read from file\n");
-
-        order.Quantity = fixQty;
-        order.ExecutedQuantity = fixExecQty;
-        order.Price = fixPrice;
-        instruction = fixState; // TO BE IMPLEMENTED: Set the state of the order using the first line of FIXLog.txt \\ order.state = fixState;
-
-            switch(instruction)
+            var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            
+            try
             {
-                case "PendingNew ":
-                    order.StatePendingNew(order);
-                    break;
-                case "New ":
-                    order.StateNew(order);
-                    break;
-                case "PartiallyFilled ":
-                    order.StatePartiallyFilled(order);
-                    break;
-                case "Filled ":
-                    order.StateFilled(order);
-                    break;
-                case "DoneForDay ":
-                    order.StateDoneForDay(order);
-                    break;
-                case "PendingCancel ":
-                    order.StatePendingCancel(order);
-                    break;
-                case "PendingReplace ":
-                    order.StatePendingReplace(order);
-                    break;
-                case "Canceled ":
-                    order.StateCanceled(order);
-                    break;
-                case "Rejected ":
-                    order.StateRejected(order);
-                    break;
-                case "Stopped ":
-                    order.StateStopped(order);
-                    break;
-                default:
-                    WriteLine("Invalid workflow.");
-                    break;
+                using (var sr = new StreamReader(fs))
+                {
+                    for (int i = 0; i < lineCount; i++)
+                    {
+                        sr.ReadLine();
+                    }
+                    lines = sr.ReadLine();
+                }
+            }
+            catch (IOException e)
+            {
+                WriteLine("There was an error reading the file:");
+                WriteLine(e.Message);
+            }
+        
+            if (lines != null){
+             //Do fix message validation
+
+                lineCount++;
+
+                string fixState = Regex.Match(lines,@"(?<=State: ).+?(?=\|)").ToString();
+                var fixQty = int.Parse(Regex.Match(lines,@"(?<=Quantity: ).+?(?=\|)").ToString());
+                var fixExecQty = int.Parse(Regex.Match(lines,@"(?<=Executed Quantity: ).+?(?=\|)").ToString());
+                var fixPrice = 0;
+
+                bool priceCheck;
+                priceCheck = int.TryParse(Regex.Match(lines,@"(?<=Price: ).+?(?=\|)").ToString(),out fixPrice);
+
+                if(!priceCheck){
+                    WriteLine("Price not in correct format, setting to 0.");
+                    fixPrice = 0;
+                }
+
+                System.Console.WriteLine("State: " + fixState);
+                System.Console.WriteLine("Quantity: " + fixQty);
+                System.Console.WriteLine("Executed Quantity: " + fixExecQty);
+                System.Console.WriteLine("Price: " + fixPrice + " <========= read from file\n");
+
+                order.Quantity = fixQty;
+                order.ExecutedQuantity = fixExecQty;
+                order.Price = fixPrice;
+                instruction = fixState; // TO BE IMPLEMENTED: Set the state of the order using the first line of FIXLog.txt \\ order.state = fixState;
+
+                    switch(instruction)
+                    {
+                        case "PendingNew ":
+                            order.StatePendingNew(order);
+                            break;
+                        case "New ":
+                            order.StateNew(order);
+                            break;
+                        case "PartiallyFilled ":
+                            order.StatePartiallyFilled(order);
+                            break;
+                        case "Filled ":
+                            order.StateFilled(order);
+                            break;
+                        case "DoneForDay ":
+                            order.StateDoneForDay(order);
+                            break;
+                        case "PendingCancel ":
+                            order.StatePendingCancel(order);
+                            break;
+                        case "PendingReplace ":
+                            order.StatePendingReplace(order);
+                            break;
+                        case "Canceled ":
+                            order.StateCanceled(order);
+                            break;
+                        case "Rejected ":
+                            order.StateRejected(order);
+                            break;
+                        case "Stopped ":
+                            order.StateStopped(order);
+                            break;
+                        default:
+                            WriteLine("Invalid workflow.");
+                            break;
+                    }
+
+                WriteLine("-----------------------------Execution Report---------------------------------");
+                WriteLine(order);
+                WriteLine("------------------------------------------------------------------------------\n");
+
             }
 
-        WriteLine("-----------------------------Execution Report---------------------------------");
-        WriteLine(order);
-        WriteLine("------------------------------------------------------------------------------\n");
-            
-        ReadLine();
+            else {
+                wh.WaitOne(1000);
+                }
         }
+        wh.Close();
     }
 }
